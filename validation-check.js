4890d4ab9c1ff45e7d90ffd0891290d235d32eb6
Mikolaj Kalwa <mikolaj.kalwa@codetain.com>
2023-11-19 11:48:52 +0100

    add myzod validation

--- validation-check.js
+++ validation-check.js
@@ -1,9 +1,13 @@
 import { user } from './src/fixture.js'
 import * as ajv from './src/schemas/ajv.js'
 import * as joi from './src/schemas/joi.js'
+import * as myzod from './src/schemas/myzod.js'
 
 console.log('ajv base', ajv.baseSchema(user))
 console.log('ajv details', ajv.detailsSchema(user))
 
 console.log('joi base', joi.baseSchema.validate(user))
 console.log('joi details', joi.detailsSchema.validate(user))
+
+console.log('myzod base', myzod.baseSchema.parse(user))
+console.log('myzod details', myzod.detailsSchema.parse(user))
