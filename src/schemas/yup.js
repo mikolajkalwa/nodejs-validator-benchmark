3921bb41a543418d63cca17ad9c580b19c7c2d2a
Mikolaj Kalwa <mikolaj.kalwa@codetain.com>
2023-11-19 11:57:10 +0100

    add yup validation

---
+++ src/schemas/yup.js
@@ -0,0 +1,28 @@
+import yup from 'yup'
+
+export const baseSchema = yup.object().shape({
+  name: yup.object().shape({
+    first: yup.string(),
+    last: yup.string()
+  }),
+  login: yup.object().shape({
+    email: yup.string(),
+    password: yup.string(),
+  }),
+  organization_id: yup.string(),
+  requested_at: yup.string(),
+})
+
+
+export const detailsSchema = yup.object().shape({
+  name: yup.object().shape({
+    first: yup.string().min(1).max(999),
+    last: yup.string().min(1).max(999),
+  }),
+  login: yup.object().shape({
+    email: yup.string().email(),
+    password: yup.string().min(12).max(50),
+  }),
+  organization_id: yup.string().uuid(),
+  requested_at: yup.date(),
+})
--- validation-check.js
+++ validation-check.js
@@ -2,6 +2,7 @@ import { user } from './src/fixture.js'
 import * as ajv from './src/schemas/ajv.js'
 import * as joi from './src/schemas/joi.js'
 import * as myzod from './src/schemas/myzod.js'
+import * as yup from './src/schemas/yup.js'
 
 console.log('ajv base', ajv.baseSchema(user))
 console.log('ajv details', ajv.detailsSchema(user))
@@ -11,3 +12,6 @@ console.log('joi details', joi.detailsSchema.validate(user))
 
 console.log('myzod base', myzod.baseSchema.parse(user))
 console.log('myzod details', myzod.detailsSchema.parse(user))
+
+console.log('yup base', yup.baseSchema.validateSync(user))
+console.log('yup details', yup.detailsSchema.validateSync(user))
