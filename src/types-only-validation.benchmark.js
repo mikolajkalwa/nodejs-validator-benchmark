24286b53158e6c7ac1740ec457cebcc3a1e0ccb3
Mikolaj Kalwa <mikolaj.kalwa@codetain.com>
2023-11-23 20:56:55 +0100

    split types only and full benchmark results and implementation

--- src/types-only-validation.benchmark.js
+++ src/types-only-validation.benchmark.js
@@ -7,43 +7,24 @@ import * as yup from './schemas/yup.js'
 import * as zod from './schemas/zod.js'
 
 cronometro({
-  'ajv - types only': function () {
+  ajv: function () {
     ajv.baseSchema(user)
   },
-  'ajv - full validation': function () {
-    ajv.detailsSchema(user)
-  },
-
-  'joi - types only': function () {
+  joi: function () {
     joi.baseSchema.validate(user)
   },
-  'joi - full validation': function () {
-    joi.detailsSchema.validate(user)
-  },
-
-  'myzod - types only': function () {
+  myzod: function () {
     myzod.baseSchema.parse(user)
   },
-  'myzod - full validation': function () {
-    myzod.detailsSchema.parse(user)
-  },
-
-  'yup - types only': function () {
+  yup: function () {
     yup.baseSchema.validateSync(user)
   },
-  'yup - full validation': function () {
-    yup.detailsSchema.validateSync(user)
-  },
-
-  'zod - types only': function () {
+  zod: function () {
     zod.baseSchema.parse(user)
-  },
-  'zod - full validation': function () {
-    zod.detailsSchema.parse(user)
   }
 }, {
   errorThreshold: 0,
-  iterations: 1_000_000,
+  iterations: 10_000_000,
   print: {
     colors: false
   }
